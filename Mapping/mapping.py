154af067fd287e2e33a938a7d7f196131c086120
Bharath5324 <bharath.s@ieee.org>
2020-02-06 22:35:34 +0530

    Beautification of the pop-ups

--- Mapping/mapping.py
+++ Mapping/mapping.py
@@ -4,10 +4,15 @@ data = pandas.read_csv("Volcanoes.csv")
 lat = data["LAT"]
 lon = data["LON"]
 elev = data["ELEV"]
+name = data["NAME"]
 map = folium.Map(location = [48,-121], zoom_start = 20, tiles = "Stamen Terrain")
+html = """Volcano name : <br>
+<a href="https://www.google.com/search?q=%%22%s%%22" target = "_blank">%s</a><br>
+Height : %s m
+"""
 fg = folium.FeatureGroup(name = "My Map")
-for lt, ln, el in zip(lat, lon, elev):
-    popup = folium.Popup(str(el) + " m", parse_html = True)
-    fg.add_child(folium.Marker(location = [lt, ln], popup = popup, icon = folium.Icon(color = "green")))
+for lt, ln, el, nam in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(html=html % (nam, nam, el), width=200, height=100)
+    fg.add_child(folium.Marker(location = [lt, ln], popup = folium.Popup(iframe), icon = folium.Icon(color = "green")))
 map.add_child(fg)
 map.save("Map1.html")
